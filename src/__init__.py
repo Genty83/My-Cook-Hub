d8668d68602c5e43e94fc518c293083de6c1ef56
Craig Gent <genty83@outlook.com>
2024-06-29 14:49:06 +0000

    Added mongo variable

--- src/__init__.py
+++ src/__init__.py
@@ -3,8 +3,15 @@
 import os 
 from flask import Flask
 from flask_pymongo import PyMongo
+if os.path.exists("env.py"):
+    import env
 
 # Create flask instance
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+
+
+# Database instance
+mongo = PyMongo(app)
